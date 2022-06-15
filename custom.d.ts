38fdb1675a834e3d604240aabc7b66ad1dbfce2e
OmkarAvalor <omkar@avalor.investments>
2022-06-15 12:23:11 +0530

    add video

---
+++ custom.d.ts
@@ -0,0 +1,5 @@
+// declare module "roe.mp4" // e.g declare module 'react-leaflet'
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+  }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <video
+          // src="https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4"
+          src="roe.mp4"
+          width="600"
+          height="300"
+          autoPlay={true}
+          controls={true}
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
